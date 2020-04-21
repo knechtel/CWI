3e7a76653389a88bb06ebce249282f824c3c2c3d
Maiquel Knechtel <maiquelknechtel@gmail.com>
2020-04-21 07:27:43 -0300

    Adicionando validador de CPF.

--- src/main/java/com/cwi/demo/controller/AssociadoController.java
+++ src/main/java/com/cwi/demo/controller/AssociadoController.java
@@ -2,6 +2,7 @@ package com.cwi.demo.controller;
 
 
 import com.cwi.demo.DTO.AssociadoDTO;
+import com.cwi.demo.DTO.UserDTO;
 import com.cwi.demo.service.AssociadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -19,9 +20,13 @@ public class AssociadoController {
     private AssociadoService associadoService;
 
     @RequestMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public HttpEntity<?> save(@RequestBody AssociadoDTO associadoDTO) {
-        associadoService.save(associadoDTO.build());
-        return ResponseEntity.ok().build();
+    public UserDTO save(@RequestBody AssociadoDTO associadoDTO) {
+        if (associadoService.validaCPF(associadoDTO)) {
+            associadoService.save(associadoDTO.build());
+            return new UserDTO("ABLE_TO_VOTE");
+        } else {
+            return new UserDTO("UNABLE_TO_VOTE");
+        }
 
     }
 }
