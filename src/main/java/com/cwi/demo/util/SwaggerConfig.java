7bc552c3ed74a4fe0a109726ccb450a7fad9d4a9
Maiquel Knechtel <maiquelknechtel@gmail.com>
2020-04-21 13:32:07 -0300

    Adicionado pacote com end-point a ser mapeado.

--- src/main/java/com/cwi/demo/util/SwaggerConfig.java
+++ src/main/java/com/cwi/demo/util/SwaggerConfig.java
@@ -16,7 +16,6 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cwi.demo.controller"))
-
                 .paths(PathSelectors.any())
                 .build();
     }
